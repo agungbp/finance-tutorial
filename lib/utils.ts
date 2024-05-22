a2cc97b68e581c64bca48ed16d83ad37b79aadf3
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-05-22 11:58:06 +0700

    create summary api

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import exp from "constants"
+import { eachDayOfInterval, isSameDay } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -20,4 +21,44 @@ export function formatCurrency(value: number) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(value)
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100
+  }
+
+  return ((current - previous) / previous) * 100
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date
+    income: number
+    expenses: number
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return []
+  }
+
+  const allDays = eachDayOfInterval({ start: startDate, end: endDate })
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day))
+
+    if (found) {
+      return found
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0
+      }
+    }
+  })
+
+  return transactionsByDay
 }
